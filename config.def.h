8d03f5fe7f0bac6f5acd779bd85a7aece8ed46d1
Heinrich <heinrich.zurhorstmeyer@gmx.de>
2024-03-24 18:17:25 +0100

    change keybindings

--- config.def.h
+++ config.def.h
@@ -62,40 +62,45 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/*modifier         , key                      , function       , argument*/
+
+	// window management
+	{ MODKEY|ShiftMask , XK_q                     , killclient     , {0} },
+	{ MODKEY|ShiftMask , XK_e                     , quit           , {0} },
+	TAGKEYS(XK_1       , 0)
+	TAGKEYS(XK_2       , 1)
+	TAGKEYS(XK_3       , 2)
+	TAGKEYS(XK_4       , 3)
+	TAGKEYS(XK_5       , 4)
+	TAGKEYS(XK_6       , 5)
+	TAGKEYS(XK_7       , 6)
+	TAGKEYS(XK_8       , 7)
+	TAGKEYS(XK_9       , 8)
+	{ MODKEY           , XK_period                , focusmon       , {.i = +1} }, // move focus one monitor up
+	{ MODKEY           , XK_comma                 , focusmon       , {.i = -1} }, // move focus one monitor down
+	{ MODKEY           , XK_j                     , focusstack     , {.i = +1} }, // move focus one window up in stack
+	{ MODKEY           , XK_k                     , focusstack     , {.i = -1} }, // move focus one window down in stack
+	{ MODKEY           , XK_plus                  , incnmaster     , {.i = +1} }, // add one window from number of master windows
+	{ MODKEY           , XK_minus                 , incnmaster     , {.i = -1} }, // remove one window from number of master windows
+	{ MODKEY           , XK_space                 , setlayout      , {0} }, // switch to last layout
+	{ MODKEY           , XK_t                     , setlayout      , {.v = &layouts[0]} }, // switch to "tile" layout
+	{ MODKEY           , XK_f                     , setlayout      , {.v = &layouts[1]} }, // switch to floating
+	{ MODKEY           , XK_o                     , setlayout      , {.v = &layouts[2]} }, // switch to "monocle" layout
+	{ MODKEY           , XK_l                     , setmfact       , {.f = +0.025} }, // increase percentage of master window on monitor
+	{ MODKEY           , XK_h                     , setmfact       , {.f = -0.025} }, // decrease percentage of master window on monitor
+	{ MODKEY|ShiftMask , XK_period                , tagmon         , {.i = +1} }, // move window tag one monitor up
+	{ MODKEY|ShiftMask , XK_comma                 , tagmon         , {.i = -1} }, // move window tag one monitor down
+	{ MODKEY|ShiftMask , XK_0                     , tag            , {.ui = ~0} }, // tag window in all tags
+	{ MODKEY|ShiftMask , XK_b                     , togglebar      , {0} },
+	{ MODKEY|ShiftMask , XK_space                 , togglefloating , {0} },
+	{ MODKEY           , XK_Tab                   , view           , {0} }, // move to last tag
+	{ MODKEY           , XK_0                     , view           , {.ui = ~0} }, // view all tags
+	{ MODKEY|ShiftMask , XK_Return                , zoom           , {0} }, // change stack master
+
+	// utility shortcuts
+	{ MODKEY           , XK_Return                , spawn          , {.v = termcmd} },
+	{ 0                , XK_Menu                  , spawn          , {.v = termcmd} },
+
 };
 
 /* button definitions */
